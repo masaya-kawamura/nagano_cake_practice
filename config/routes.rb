7ecc1277b914f7932d836f9959749eeb5320f7ba
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-13 14:17:33 +0000

    [ルーティングの分岐]adminとpublicにルーティングを分岐できるようにroutes.rbとcontrollerのディレクトリ構造を分岐しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,42 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #管理者側の認証用ルーティング
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  #お客さん用の認証用ルーティング
+  devise_for :customers,
+  path: '',
+  path_names: {
+    sign_up: '',
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup',
+  },
+  controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
+  #会員側のページ
+  namespace :public do
+    resources :customer
+    get 'top' => 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
+  #管理者側のページ
+  namespace :admin do
+    resources :customer
+    get 'top' => 'homes#top'
+  end
+
+  #会員側のurlからpublicを無くす
+  scope module: :public do
+    get 'index' => 'products#index'
+
+  end
+
 end
